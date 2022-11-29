2cee85a20d1be432fb24f334681282c7802427a6
Moise-code <moisewowe@gmail.com>
2022-11-29 14:42:36 +0200

    updating the api set up

--- src/modules/APIs.js
+++ src/modules/APIs.js
@@ -1,13 +1,5 @@
 import { nutrients } from './food.js';
 
-const commentApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
-const commentkey = 'jUTHHFhjhjmbJGYTMBJgyj';
-const commentUrl = `${commentApi}/apps/:${commentkey}/comments/`;
-
-const likeapi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
-const likekey = 'ahggGFtfjsuJHUTHhfyFHJ';
-const likeurl = `${likeapi}/apps/:${likekey}/likes/`;
-
 const mealApi = 'www.themealdb.com/api/json/v1/1/search.php?s';
 
 const mealApiObj = [];
