001b2aa87add7e9bd948631ca725ce9a1035f0b3
Moise BYIRINGIRO <moisewowe@gmail.com>
2022-11-29 17:30:53 +0200

    Update APIs.js
    updating API.js file

--- src/modules/APIs.js
+++ src/modules/APIs.js
@@ -2,11 +2,6 @@ import nutrients from './food.js';
 
 const foodApi = 'https://themealdb.com/api/json/v1/1/search.php?s';
 
-const apiId = 'PMbretgdfoefugbh';
-const commentLink = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiId}/comments`;
-
-const likeLink = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiId}/likes`;
-
 const mealApiObj = [];
 
 nutrients.forEach((food) => {
