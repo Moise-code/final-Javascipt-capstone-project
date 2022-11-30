f5d94dbdc6ef51e6d7d7a54c11b72451c31e147f
Precious Nhlapho <pnhlapho59@gmail.com>
2022-11-30 12:30:33 +0200

    Add comment fuctionality, markup and style

--- src/modules/APIs.js
+++ src/modules/APIs.js
@@ -1,11 +1,11 @@
-import nutrients from './food.js';
+// import nutrients from './food.js';
 
 const foodApi = 'https://themealdb.com/api/json/v1/1/search.php?s';
 
 const mealApiObj = [];
 
-nutrients.forEach((food) => {
-  mealApiObj.push(`${foodApi}=${food}`);
-});
+// foodApi.forEach((food) => {
+mealApiObj.push(`${foodApi}`);
+// });
 
 export default mealApiObj;
--- src/modules/displayfood.js
+++ src/modules/displayfood.js
@@ -1,10 +1,11 @@
 import mealApiObj from './APIs.js';
 
 const main = document.querySelector('main');
+const popup = document.querySelector('#showComments');
 
 /* eslint-disable */
 export const display = async () => {
-/* eslint-enable */
+  /* eslint-enable */
   // let markup = '';
 
   mealApiObj.forEach(async (item) => {
@@ -38,9 +39,44 @@ export const display = async () => {
                 <button class="button">Comments</button>
                 <button class="button">Reservations</button>
             </section>`;
-
-        // renderLikes(item.idMeal);
-        // return 'done';
+        const button = document.querySelector('.button');
+        button.addEventListener('click', () => {
+          const pop = () => {
+            popup.innerHTML = `
+            <div class="container">
+              <div class ="flex pop column">
+               <span class="close-popup">&times;</span>
+               <div class="image_container">
+                  <img class="popImage" src="${item.strMealThumb}" alt="asdf">
+                </div>
+                <p class="popMeal">Name: ${item.strMeal}</p> 
+                <p> Location: ${item.strArea}</p>
+                <p> Category: ${item.strCategory}</p>
+                <p class="instructions">${item.strInstructions}<p>
+                <div class="comment_count">
+                </div>
+                <button class="refresh_comment">Refresh Comments</button>
+                <div class="pop_comment">
+                
+                </div>
+                <div id ="commentContainer"></div>
+                  <h3 class="add-comment">Add a comment</h3>
+                <div class="input_comment">
+                  <form class="form">
+                    <input id="name"  type="text" name="user" required placeholder="Your Name"><br>
+                    <textarea id="text" type="text" rows=6 name="text" required placeholder="Your insight"></textarea><br>
+                    <div>
+                    <button class="submit-btn" type="submit">Comment</button>
+                  </div>
+                  </form>
+                </div>
+              </div>
+              </div>`;
+            popup.style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+          };
+          pop();
+        });
       });
     } catch (err) {
       main.innerHTML = 'err';
