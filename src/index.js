08482c43815b0d36c29585ec25e1b98c102d005b
Moise-code <moisewowe@gmail.com>
2022-12-01 10:17:02 +0200

    updating the index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './main.css';
 import display from './modules/displayfood.js';
+import {mealApiObj}  from './modules/APIs.js';
+const itemCounter = document.querySelector('.item_count');
+itemCounter.innerHTML = `Total Items: ${mealApiObj.length + 1}`;
 
 display();
