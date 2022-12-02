e79988fcd9c4041f8718607bdaf881ab85ead308
Moise-code <moisewowe@gmail.com>
2022-12-02 10:48:16 +0200

    adding the counter update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './main.css';
 import display from './modules/displayfood.js';
-import {mealApiObj}  from './modules/APIs.js';
+import { mealApiObj } from './modules/APIs.js';
+
 const itemCounter = document.querySelector('.item_count');
 
-itemCounter.innerHTML = `Total Items: ${mealApiObj.length}`;
+itemCounter.innerHTML = `Total Items: ${mealApiObj.length + 1}`;
 
 display();
