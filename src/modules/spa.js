10790d99ed6b879bed82bad5de212513c6f94a3c
Moise-code <moisewowe@gmail.com>
2022-12-02 14:31:22 +0200

    fixing the lynters error

--- src/modules/spa.js
+++ src/modules/spa.js
@@ -5,19 +5,18 @@ export default class Showsection {
       document.getElementById('about-section').classList.add('non-show');
       document.getElementById('contact-section').classList.add('non-show');
     };
-  
+
     // shows only about section
     static showabout = () => {
       document.getElementById('about-section').classList.remove('non-show');
       document.getElementById('show-cards').classList.add('non-show');
       document.getElementById('contact-section').classList.add('non-show');
     };
-  
+
     // shows only contact section
     static showcontact = () => {
       document.getElementById('contact-section').classList.remove('non-show');
       document.getElementById('show-cards').classList.add('non-show');
       document.getElementById('about-section').classList.add('non-show');
     };
-  }
-  
+}
